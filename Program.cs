8c7a229fab06cd40d3ed4e74f8d5fdae3d1bea26
Starzen Garters <starzen@blockquote.rip>
2024-02-23 19:59:01 -0500

    Bah, forgot to commit the startup changes. Injects a FIABContext Migrates the DB on program startup.

--- Program.cs
+++ Program.cs
@@ -1,23 +1,42 @@
 using FIAB.Contexts;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddDbContext<FIABContext>(options =>
 {
-    var cs = FIAB.Models.Utilities.Env("ConnectionString", true, builder.Configuration);
-    FIABContext.UpdateOptions(options, cs);
+	var cs = FIAB.Models.Utilities.Env("ConnectionString", true, builder.Configuration);
+	FIABContext.UpdateOptions(options, cs);
 });
 
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
+
+/* Migrate DB to latest */
+try
+{
+	Console.WriteLine($"Creating database context for migration...");
+	using (var context = FIABContext.Create(builder.Configuration))
+	{
+		Console.WriteLine($"\tMigrating database...");
+		context.Database.Migrate();
+		Console.Error.WriteLine($"\tMigrated database.");
+	}
+}
+catch (Exception ex)
+{
+	Console.Error.WriteLine($"Error migrating database: {ex.Message}");
+	throw new Exception($"Error migrating database.", ex);
+}
+/* End of Migrate */
 
 app.UseHttpsRedirection();
 
