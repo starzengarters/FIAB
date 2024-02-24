3dfa135b67e36fa2c631099ce3cc4dd62dc7d179
Starzen Garters <starzen@blockquote.rip>
2024-02-23 19:48:33 -0500

    Created and applied a migration. 🎉

---
+++ Migrations/20240224003255_initial.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FIAB.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Nouns",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    ShortDescription = table.Column<string>(type: "text", nullable: true),
+                    Details = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nouns", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RelationshipTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    ShortDescription = table.Column<string>(type: "text", nullable: true),
+                    ParentId = table.Column<int>(type: "integer", nullable: true),
+                    Details = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RelationshipTypes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RelationshipTypes_RelationshipTypes_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "RelationshipTypes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Relationships",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SubjectId = table.Column<int>(type: "integer", nullable: false),
+                    RelationshipTypeId = table.Column<int>(type: "integer", nullable: false),
+                    ObjectId = table.Column<int>(type: "integer", nullable: false),
+                    Started = table.Column<DateOnly>(type: "date", nullable: true),
+                    StartedIsApproximate = table.Column<bool>(type: "boolean", nullable: false),
+                    Ended = table.Column<DateOnly>(type: "date", nullable: true),
+                    EndedIsApproximate = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Relationships", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Relationships_Nouns_ObjectId",
+                        column: x => x.ObjectId,
+                        principalTable: "Nouns",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Relationships_Nouns_SubjectId",
+                        column: x => x.SubjectId,
+                        principalTable: "Nouns",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Relationships_RelationshipTypes_RelationshipTypeId",
+                        column: x => x.RelationshipTypeId,
+                        principalTable: "RelationshipTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Relationships_ObjectId",
+                table: "Relationships",
+                column: "ObjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Relationships_RelationshipTypeId",
+                table: "Relationships",
+                column: "RelationshipTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Relationships_SubjectId",
+                table: "Relationships",
+                column: "SubjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RelationshipTypes_ParentId",
+                table: "RelationshipTypes",
+                column: "ParentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Relationships");
+
+            migrationBuilder.DropTable(
+                name: "Nouns");
+
+            migrationBuilder.DropTable(
+                name: "RelationshipTypes");
+        }
+    }
+}
